1b86bbf4f494135bd11207cebddd99adbe96d197
henriqueAlgauer <algauer.henrique@gmail.com>
2024-03-09 15:38:29 -0300

    gerando numeros aleatorios

--- 01/app.js
+++ 01/app.js
@@ -1,3 +1,23 @@
 //criar um array vazio, popular ele com o menor numero até o maior numero
 //gerar um numero aleatório que será o index do array
     // -> importar função Math.random
+
+const qntnumeros = document.getElementById("quantidade")
+const numInicio = document.getElementById("de")
+const numFim = document.getElementById("ate")
+const sort = document.getElementById("btn-sortear")
+const restart = document.getElementById("btn-reiniciar")
+
+console.log(sort);
+console.log(restart);
+
+sort.addEventListener("click", ()=>{
+    const qntnum = qntnumeros.value
+    const menor = numInicio.value
+    const maior = numFim.value
+
+    for(let i=0; i<qntnum;i++){
+        let aleatorio = Math.floor(Math.random() * (menor - maior) + menor + maior);
+        console.log(aleatorio);
+    }
+})
