d9d0e5cd414f2765572cd627d47befd503a96d6d
henriqueAlgauer <algauer.henrique@gmail.com>
2024-03-09 22:29:11 -0300

    material inicial para desenvolver app.js

--- sorteador de numeros/test.js
+++
@@ -1,9 +0,0 @@
-function sortear() {
-    let quantidade = document.getElementById("quantidade").value;
-    let de = gameClicado.getElementById('de').value;
-    let ate = gameClicado.getElementById('ate').value;
-
-    alert(`Quantidade de números: ${quantidade}`);
-    alert(`Do número: ${de}`);
-    alert(`Até: ${ate}`);
-}
